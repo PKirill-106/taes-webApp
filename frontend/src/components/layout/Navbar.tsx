50d93cdc9b50ef6e4ccf6b3f00dd93b7194c6f32
Kyryl <p.kirill.design@gmail.com>
2026-02-10 17:20:15 +0100

    style: improve navbar styles

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -4,7 +4,7 @@ import NavList from './NavList'
 
 export default function Navbar() {
 	return (
-		<header className='section-container max-w-none md:mb-14'>
+		<header className='section-container py-2 max-w-none md:mb-14'>
 			<div className='flex gap-4 justify-between items-center'>
 				<div>
 					<Logo type='main' height={120} width={120} />
