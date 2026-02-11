587f5ac0efc3eb3098e75f55f447ac452e465624
Kyryl <p.kirill.design@gmail.com>
2026-02-11 11:27:31 +0100

    feat: add about us in numbers section

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,9 +1,11 @@
-import BannerSection from "@/components/main-page/banner/BannerSection";
+import AboutUsInNumbers from '@/components/main-page/about-us/AboutUsInNumbers'
+import BannerSection from '@/components/main-page/banner/BannerSection'
 
 export default function Home() {
 	return (
 		<>
 			<BannerSection />
+			<AboutUsInNumbers />
 		</>
 	)
 }
---
+++ frontend/src/components/main-page/about-us/AboutUsInNumbers.tsx
@@ -0,0 +1,39 @@
+import Section from '@/components/ui/Section'
+import { useTranslations } from 'next-intl'
+
+export default function AboutUsInNumbers() {
+	const t = useTranslations('HomePage')
+
+	const stats = t.raw('AboutUsInNumbers') as Array<{
+		number: string
+		title: string
+		descriptioin: string
+	}>
+
+	const startYear = 2009
+	const currentYear = new Date().getFullYear()
+	const yearsOfExperience = currentYear - startYear
+
+	return (
+		<Section>
+			<div className='flex flex-col md:flex-row text-center md:text-start md:items-baseline justify-between gap-8 md:gap-4'>
+				{stats.map((item, index) => (
+					<div
+						key={index}
+						className='flex items-center md:items-baseline flex-col gap-3 md:gap-0 w-full md:max-w-65'
+					>
+						<span className='text-6xl md:text-4xl lg:text-6xl font-bold text-primary max-w-50 w-full md:w-auto md:max-w-auto'>
+							{index == 0 ? yearsOfExperience : item.number}
+						</span>
+						<div className='flex flex-col justify-center'>
+							<span className='text-xl md:text-2xl font-bold'>
+								{item.title}
+							</span>
+							<span className='text-base font-light'>{item.descriptioin}</span>
+						</div>
+					</div>
+				))}
+			</div>
+		</Section>
+	)
+}
