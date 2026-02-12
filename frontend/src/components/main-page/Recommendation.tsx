7e7370242a51e516b8c6fc01b24845e3cd25c402
Kyryl <p.kirill.design@gmail.com>
2026-02-12 13:04:23 +0100

    feat: add link button in recommendation section

--- frontend/src/components/main-page/Recommendation.tsx
+++ frontend/src/components/main-page/Recommendation.tsx
@@ -1,6 +1,8 @@
 import { useTranslations } from 'next-intl'
 import Section from '../ui/Section'
 import RecommendationItem from '../ui/RecommendationItem'
+import Link from 'next/link'
+import { ArrowUpRight } from 'lucide-react'
 
 export default function Recommendation() {
 	const t = useTranslations('HomePage.Recommendations')
@@ -11,7 +13,16 @@ export default function Recommendation() {
 			className='flex flex-col items-center gap-15'
 		>
 			<h3>{t('title')}</h3>
-			<RecommendationItem />
+			<div className='space-y-3 md:space-y-6'>
+				<RecommendationItem />
+				<Link
+					href='/recommendations'
+					className='flex items-center gap-2 w-full justify-end hover-active-text'
+				>
+					{t('linkText')}{' '}
+					<ArrowUpRight className='size-4 stroke-2 group-hover: duration-200' />
+				</Link>
+			</div>
 		</Section>
 	)
 }
