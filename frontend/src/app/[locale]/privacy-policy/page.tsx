1721f4894ebbfc4d9c05b54b49551f1e62fd6287
Kyryl <p.kirill.design@gmail.com>
2026-02-24 18:15:18 +0100

    feat: add metadata to privacy page

--- frontend/src/app/[locale]/privacy-policy/page.tsx
+++ frontend/src/app/[locale]/privacy-policy/page.tsx
@@ -1,32 +1,27 @@
-'use client'
-import Section from '@/components/ui/Section'
-import { Skeleton } from '@/components/ui/skeleton'
-import { renderPrivacyPolicy } from '@/lib/utils/helpers'
-import { useGetPrivacyQuery } from '@/state/privacy/privacyApiSlice'
-import { useLocale } from 'next-intl'
+import PrivacyPolicyClientPage from '@/components/privacy-policy/PrivacyPolicyClientPage'
+import { buildMetadata } from '@/lib/seo/buildMetadata';
+import { getTranslations } from 'next-intl/server';
+
+type Props = {
+	params: Promise<{ slug: string; locale: string }>
+}
+
+export async function generateMetadata({ params }: Props) {
+	const { locale } = await params
+
+	const t = await getTranslations({
+		locale,
+		namespace: 'Metadata.PrivacyPage',
+	})
+
+	return buildMetadata({
+		title: t('title'),
+		description: t('description'),
+		locale: locale,
+		path: '/contacts',
+	})
+}
 
 export default function PrivacyPolicyPage() {
-	const locale = useLocale()
-	const { data: privacy, isLoading, isError } = useGetPrivacyQuery(locale)
-
-	if (isLoading) {
-		return (
-			<Section className='py-20'>
-				<Skeleton className='h-10 w-1/3 mb-10 mx-auto' />
-				<div className='space-y-4'>
-					<Skeleton className='h-4 w-full' />
-					<Skeleton className='h-4 w-full' />
-					<Skeleton className='h-4 w-3/4' />
-				</div>
-			</Section>
-		)
-	}
-
-	if (isError || !privacy) return null
-
-	return (
-		<Section className='py-20 max-w-4xl mx-auto'>
-			{renderPrivacyPolicy(privacy.privacy_policy)}
-		</Section>
-	)
+	return <PrivacyPolicyClientPage />
 }
