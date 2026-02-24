1bfdc827034638043a9883ad2a32b6b0f958a584
Kyryl <p.kirill.design@gmail.com>
2026-02-24 18:13:40 +0100

    feat: add metadata to about us page

--- frontend/src/app/[locale]/about/page.tsx
+++ frontend/src/app/[locale]/about/page.tsx
@@ -1,5 +1,24 @@
 import AboutInfo from '@/components/about-page/AboutInfo'
 import CertificateList from '@/components/about-page/CertificateList'
+import { buildMetadata } from '@/lib/seo/buildMetadata'
+import { getTranslations } from 'next-intl/server'
+
+type Props = {
+	params: Promise<{ slug: string; locale: string }>
+}
+
+export async function generateMetadata({ params }: Props) {
+	const { locale } = await params
+
+	const t = await getTranslations({ locale, namespace: 'Metadata.AboutPage' })
+
+	return buildMetadata({
+		title: t('title'),
+		description: t('description'),
+		locale: locale,
+		path: '/about',
+	})
+}
 
 export default function AboutUsPage() {
 	return (
